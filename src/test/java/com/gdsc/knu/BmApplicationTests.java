5843acccf04a4d3c66606b4f2a4d0e2eb329df24
Choi Jun Ho <jjun3511@naver.com>
2024-02-05 17:16:32 +0900

    [test] : first commit

---
+++ src/test/java/com/gdsc/knu/BmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.knu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
