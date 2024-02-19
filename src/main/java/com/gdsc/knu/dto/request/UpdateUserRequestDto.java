318d0e9aa3b5d497c3f859d540624ace511bd2ee
Choi Jun Ho <jjun3511@naver.com>
2024-02-19 22:37:14 +0900

    [fix] : Request Body error 해결.

--- src/main/java/com/gdsc/knu/dto/request/UpdateUserRequestDto.java
+++ src/main/java/com/gdsc/knu/dto/request/UpdateUserRequestDto.java
@@ -2,9 +2,11 @@ package com.gdsc.knu.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UpdateUserRequestDto {
     private Long id;
     private String nickname;
