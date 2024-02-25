74f7670ac434020e6a53bf4d02d58328cc438e7b
Choi Jun Ho <jjun3511@naver.com>
2024-02-25 21:42:53 +0900

    [feat] : 해양 생물 prompt 설정.

--- src/main/java/com/gdsc/knu/dto/MarineApiResultDto.java
+++ src/main/java/com/gdsc/knu/dto/MarineApiResultDto.java
@@ -6,8 +6,16 @@ import java.util.List;
 
 @Getter
 public class MarineApiResultDto {
-    private List<String> name;
+    // 해양 생물의 종류와 개수를 저장하는 DTO
+    private int numberOfMarineLife;
+    private int numberOfSpecies;
 
     public MarineApiResultDto(String text) {
+        this.numberOfMarineLife = 0;
+        String[] marineLife = text.split(",");
+        this.numberOfSpecies = marineLife.length;
+        for (String s : marineLife) {
+            this.numberOfMarineLife += Integer.parseInt(s.split(":")[1].trim());
+        }
     }
 }
