658e1e365540afa71946eb4ea878dfa48140aa31
youngin kim <hardworkingini@gmail.com>
2024-02-26 08:15:22 +0900

    webp 파일로 변환

--- src/main/java/com/gdsc/knu/controller/UserController.java
+++ src/main/java/com/gdsc/knu/controller/UserController.java
@@ -82,7 +82,7 @@ public class UserController {
         userDto.setLogin(false);
         userDto.setRegion("kr");
         userDto.setDeleted(false);
-        userDto.setProfileImageUrl("https://mblogthumb-phinf.pstatic.net/MjAyMDExMDFfMyAg/MDAxNjA0MjI5NDA4NDMy.5zGHwAo_UtaQFX8Hd7zrDi1WiV5KrDsPHcRzu3e6b8Eg.IlkR3QN__c3o7Qe9z5_xYyCyr2vcx7L_W1arNFgwAJwg.JPEG.gambasg/%EC%9C%A0%ED%8A%9C%EB%B8%8C_%EA%B8%B0%EB%B3%B8%ED%94%84%EB%A1%9C%ED%95%84_%ED%8C%8C%EC%8A%A4%ED%85%94.jpg?type=w800");
+        userDto.setProfileImageUrl("https://lh3.googleusercontent.com/a/ACg8ocLEBpTuQ27MOubGP9_jt2jGhQFDvvC7eujuRaP-r52Z=s96-c");
 
         userService.createDummyUser(userDto);
 
